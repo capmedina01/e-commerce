ca32aaafcbfb9717062a5a7dd361a5ec4640ba55
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-09-19 16:58:45 -0500

    creando boton para añadir productos

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 const container = document.getElementById("container-cards");
 const url = "https://fakestoreapi.com/products";
+const cartIcon = document.getElementById("cart-icon");
+const cartCount = document.getElementById("cart-count");
+
 /*window.addEventListener("DOMContentLoaded", );*/
 
 const getAllProducts = async (url) => {
@@ -12,11 +15,14 @@ const getAllProducts = async (url) => {
   });
 };
 getAllProducts(url);
+updateCartCount();
 
 function createCard(element) {
   const divCard = document.createElement("div");
   const divImg = document.createElement("div");
   const divDescription = document.createElement("div");
+  const addToCartButton = document.createElement("button");
+
   divCard.classList.add("divCards");
   divImg.classList.add("divImg");
   divDescription.classList.add("divDescription");
@@ -35,12 +41,16 @@ function createCard(element) {
   const categoryCard = document.createElement("h4");
   categoryCard.textContent = element.category;
 
+  addToCartButton.textContent = "Añadir al carrito";
+  addToCartButton.addEventListener("click", () => addToCart(product));
+
   divImg.appendChild(imgCard);
   divDescription.appendChild(description);
   divDescription.appendChild(price);
   divDescription.appendChild(categoryCard);
   divCard.appendChild(divImg);
   divCard.appendChild(divDescription);
+  divCard.appendChild(addToCartButton);
 
   container.appendChild(divCard);
 }
